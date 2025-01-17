bf46b82987b6af07426f7eed203234b85e2e4ad4
Raushan7667 <kumarraushan.aic@gmail.com>
2025-01-17 23:32:52 +0530

    Home Screen for Information Page

---
+++ src/Component/HomePage/Home.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react'
+import Typed from 'typed.js';
+import HeroSection from './HeroSection';
+import Service from './Service';
+import Technique from './Technique';
+
+const Home = () => {
+
+  useEffect(() => {
+    // Initialize Typed.js after the component mounts
+    const typed = new Typed(".role", {
+      strings: [
+        "Empowering Farmers, Enriching Futures",
+        "Sustainable Farming Solutions for a Greener Tomorrow",
+        "Connecting Farmers with Technology and Innovation",
+        "From Seed to Harvest, We Are With You Every Step",
+        "Quality Products, Better Harvests",
+        "Farming Made Simple with Technology",
+      ],
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 80,
+      backDelay: 1000,
+    });
+
+    // Clean up Typed.js on component unmount
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+
+
+  return (
+    <div>
+      <div className="relative w-full h-[70vh] overflow-hidden mt-16">
+        <video
+          src="/Assets/Video/Video Banner.mp4"
+          autoPlay
+          loop
+          muted
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        >
+          Your browser does not support the video tag.
+        </video>
+        <div className='absolute z-10 mt-20 font-bold text-3xl px-32 gap-32 text-white'>
+          <div className=''>
+            <div>Powerful Digital</div>
+            <div className='mt-2'> Agriculture Solutions</div>
+          </div>
+          <div className='mt-10'>
+            <div >We are a company dedicated to revolutionizing the</div>
+            <div className='mt-2'>agricultural industry we provide  <span className="role text-green-600"></span></div>
+          </div>
+
+        </div>
+      
+
+      </div>
+      <div className=''>
+      <HeroSection/>
+
+      </div>
+      <div className='px-32'>
+      <Service/>
+          
+      </div>
+      <div className='px-32'>
+        <Technique/>
+      </div>
+     
+     
+  
+     
+  
+
+
+    </div>
+  )
+}
+
+export default Home
