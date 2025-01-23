8464f6789ef91811da57d95f7001a87bc64e1a5a
Raushan7667 <kumarraushan.aic@gmail.com>
2025-01-23 19:56:06 +0530

    nothing

--- src/Component/HomePage/SignUp.js
+++ src/Component/HomePage/SignUp.js
@@ -125,9 +125,9 @@ const SignUp = () => {
                             onChange={(e) => setFormData({ ...formData, accountType: e.target.value })}
                         >
                             <option value="">Select account type</option>
-                            <option value="user">user</option>
+                            <option value="user">User</option>
                             <option value="business">Farmer</option>
-                            <option value="enterprise">Seller</option>
+                            <option value="Seller">Seller</option>
                         </select>
                     </div>
 
